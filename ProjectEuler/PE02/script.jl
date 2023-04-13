fbd94c07fd797f4492c83e3b883dc6b453b6b0fa
RobertAlden <randydonst@gmail.com>
2023-04-13 13:22:48 -0500

    wow

--- ProjectEuler/PE02/script.jl
+++ ProjectEuler/PE02/script.jl
@@ -1,5 +1,15 @@
-
+# fibonacci(n) = n <= 2 ? 1 : fibonacci(n - 2) + fibonacci(n - 1)
+function fibonacci(x) 
+    a, b = 0, 1
+    for i in 1:x
+        a, b = a + b, a
+    end 
+    a
+end
 
 function main()
-    
+    for i in 1:10
+        println(i, ' ', fibonacci(i)) 
+    end
 end
+@time main()
