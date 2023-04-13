332ebc606d5103f356b8325f60b3a77d748530c9
RobertAlden <randydonst@gmail.com>
2023-04-13 11:09:44 -0500

    final

--- ProjectEuler/PE01/script.jl
+++ ProjectEuler/PE01/script.jl
@@ -1,16 +1,19 @@
 using Curry
+using Base.Iterators
 range_sum(x) = x * (x+1) / 2
 range_multiple_sum(limit, multiple::Integer) = range_sum(limit ÷ multiple) * multiple
 f(a,b) = mapreduce(curry(range_multiple_sum)(a), +, b)
 
 function products(A)
-    [cumprod(A[i:length(A)]) for i in 1:length(A)-1] |> Iterators.flatten |> collect
+    ([cumprod(A[i:length(A)]) for i in 1:length(A)-1] 
+    |> flatten 
+    |> collect)
 end
 
 function main()
     limit = 999
     values = [3, 5]
     prods = setdiff(products(values), values)
-    println(mapreduce(curry(f)(limit), -, [values, prods]))
+    mapreduce(curry(f)(limit), -, [values, prods])
 end
 @time main()
