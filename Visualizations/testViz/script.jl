5b4839c5324b066a626b013dccae6b92b6aca340
RobertAlden <randydonst@gmail.com>
2023-04-19 07:51:22 -0500

    weeeeee

--- Visualizations/testViz/script.jl
+++ Visualizations/testViz/script.jl
@@ -1,38 +1,43 @@
-using Compose, Colors, Reel, IterTools
+using AbbreviatedStackTraces, Infiltrator
+using Compose, Colors, Reel, IterTools, Base.Iterators
 import Cairo, Fontconfig
 
 xs = [];
 ys = [];
 
-function driver(t,x,d,colors)
-    speed = 1
-    max_length = 0.7
-    θ = @. speed * t * x * 2 * π
-    m = @. d * max_length
-    m[1] = 0.0
-    v = zip(cos.(θ) .* m, sin.(θ) .* m)  
-    points = collect(partition(accumulate(.+,v),2,1))
-    trace = last(points)
+function driver(t,Is,As,Ls,colors)
+    speed = 0.11
+    max_length = 0.35
+    θs = @. speed * t * (Is .+ As) * 2π
+    Ds = @. Ls * max_length
+    Ds[1] = 0.0
+    Vs = zip(cos.(θs) .* Ds, sin.(θs) .* Ds)  
+    lines = collect(IterTools.partition(accumulate(.+,Vs),2,1))
+    trace = last(lines)
     push!(xs,trace[2][1])
     push!(ys,trace[2][2])
+    xs .+= 0.015
     compose(
-        context(), 
-        (context(units=UnitBox(-1,-1,2,2)),
-            line(points),stroke("black"), linewidth(3px),
-            circle(xs,ys,[0.01]), fill(colors)))
+        context(0.05,0.05,0.9,0.9), 
+        (context(units=UnitBox(-.25,-1,2,2)),
+            line(lines),stroke("white"), linewidth(3px)),
+        (context(units=UnitBox(-.25,-1,2,2)),
+            circle(xs,ys,[0.01]),  
+            fill(collect(take(cycle(colors),length(xs))))))
 end
 
 function main()
-    dim = 512px
+    dim = 256px
     set_default_graphic_size(dim, dim)
-    figsize = 1px
 
     colors = distinguishable_colors(20)
-    n = 5
-    x = rand(Float16,n) .* 2 .- 1
-    f = rand(Float16,n)
-    film = roll(fps=30, duration=5.0) do t, dt
-        driver(t,x,f,colors)
+    n = 10
+    max_speed = 5
+    Is = rand(Float16,n) .* 2π
+    As = rand(Float16,n) .* 2max_speed .- max_speed
+    Ls = sort(rand(Float16,n),rev=true)
+    film = roll(fps=30, duration=30) do t, dt
+        driver(t,Is,As,Ls,colors)
     end
     write("output.gif", film)
 end
