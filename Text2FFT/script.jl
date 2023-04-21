d081ff4df26ef7bb0069540e36397bb7c97150d4
RobertAlden <randydonst@gmail.com>
2023-04-21 09:18:23 -0500

    wip

---
+++ Text2FFT/script.jl
@@ -0,0 +1,39 @@
+using Compose, Colors, Reel, FileIO, ImageFiltering
+import Cairo, Fontconfig
+
+rgbToGrey(rgb::RGB) = 0.299 * rgb.r + 0.587 * rgb.g + 0.114 * rgb.b
+greyToRGB(f) = RGB(f,f,f)
+threshold(x,tl,th) = x > tl ? (x < th ? x : 1) : 0
+quantize(n,v) = n / (1/v)
+
+function textToImage(txt)
+    text_composition = compose(context(),
+    (context(), 
+        text(0.5,0.6,txt,hcenter,vcenter),
+        fontsize(55px), stroke("black"), font("consolas")),
+    (context(), rectangle(),fill("white"))
+    )
+    draw(PNG("text.png",(32*length(txt))px, 64px),text_composition)
+    data = FileIO.load("text.png")
+    round.(rgbToGrey.(data))
+end
+
+function edgeDetection(im)
+    G = 1/159 .* [2 4 5 4 2; 4 9 12 9 4; 5 12 15 12 5; 4 9 12 9 4; 2 4 5 4 2]
+    I₀ = imfilter(im,G)
+    K = [1 0 -1; 2 0 -2; 1 0 -1]
+    Iᵢ = imfilter(I₀, K)
+    Iₖ = imfilter(I₀, K')
+    Iᵢₖ = @. √(Iᵢ^2 + Iₖ^2)
+    Iₐ = @. atan(Iₖ/Iᵢ)
+    Iᵩ = quantize(Iₐ,8)
+    Iₜ = threshold.(Iᵢₖ,0.99,1)
+    greyToRGB.(Iₜ)
+end
+
+function main()
+    input_text = "hello world"
+    input_text |> textToImage |> edgeDetection |> FileIO.save("out.png")
+end
+
+main()
